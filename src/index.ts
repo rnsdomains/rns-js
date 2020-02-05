392f239e7882614490739265a86fee98dc78f4ca
Javi Esses <javiesses@hotmail.com>
2020-02-05 13:28:33 -0300

    Added module.export to avoid exports.default

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import Web3 from 'web3/types';
-import { RNS, Contracts, Options, ContractAddresses, ChainId } from './types';
+import { RNS as IRNS, Contracts, Options, ContractAddresses, ChainId } from './types';
 import { createRegistry, createContractAddresses } from './factories';
 import { LIBRARY_NOT_COMPOSED } from './errors';
 import Resolutions from './resolutions';
 
-export default class implements RNS {
+class RNS implements IRNS {
   private _contracts!: Contracts;
   private _contractAddresses!: ContractAddresses;
   private _resolutionHelper!: Resolutions;
@@ -53,3 +53,6 @@ export default class implements RNS {
     }
   }
 }
+
+module.exports = RNS;
+export default RNS;

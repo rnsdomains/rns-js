d25e15d721da2123cba175a3f68868f4ad13c8c0
Javi Esses <javiesses@hotmail.com>
2020-02-05 20:53:55 -0300

    Commented exported class and public methods

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,39 @@ import { createRegistry, createContractAddresses } from './factories';
 import { LIBRARY_NOT_COMPOSED } from './errors';
 import Resolutions from './resolutions';
 
+/**
+ * RNS JavaScript library.
+ * Used to interact with the complete RNS suite.
+ */
 export = class implements RNS {
   private _contracts!: Contracts;
   private _contractAddresses!: ContractAddresses;
   private _resolutionHelper!: Resolutions;
   private _composed!: boolean;
 
+  /**
+   * RNS constructor
+   *
+   * @remarks
+   * The blockchain network will be detected automatically. In case is not RSK Mainnet or RSK Testnet, the second paramenter must be provided.
+   * 
+   * @param web3 - Web3 instance 
+   * @param options - Necessary contract addresses in case the blockchain is not RSK Mainnet or RSK Testnet
+   */
   constructor (public web3: Web3, options?: Options) {
     if(options && options.contractAddresses) {
       this._contractAddresses = options.contractAddresses
     }
   }
 
+  /**
+   * RNS suite contract instances
+   *
+   * @throws
+   * Throws an error if the library was not previously composed
+   * 
+   * @returns Object with a {@link https://web3js.readthedocs.io/en/v1.2.4/web3-eth-contract.html | web3.eth.Contract} instance for each necessary contract.
+   */
   get contracts(): Contracts {
     if(!this._contracts) {
       throw new Error(LIBRARY_NOT_COMPOSED);
@@ -23,6 +44,13 @@ export = class implements RNS {
     return this._contracts;
   }
 
+  /**
+   * Detects the current network and instances the contracts.
+   *
+   * @throws
+   * Throws an error if the network is not RSK Mainnet or RSK Testnet and the options parameter was not provided in the constructor.
+   * 
+   */
   public async compose(): Promise<void> {
     if (!this._composed) {
       await this._detectNetwork();
@@ -44,6 +72,15 @@ export = class implements RNS {
     }
   }
 
+  /**
+   * Resolves the given domain for the given blockchain. If not chainId provided, it will resolve the domain in the current blockchain
+   *
+   * @throws
+   * Throws an error when the domain doesn't have resolver, when it has an invalid resolver or if the resolution hasn't been set yet.
+   
+   * @param domain - Domain to be resolved
+   * @param chainId - Should match one of the listed in {@link https://github.com/satoshilabs/slips/blob/master/slip-0044.md | SLIP44}
+   */
   async addr(domain: string, chainId?: ChainId): Promise<string> {
     await this.compose();
     if (!chainId) {

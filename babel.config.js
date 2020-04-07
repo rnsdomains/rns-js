5ff820aecf4d8633721ba4650de9d031c053ec0f
Javi Esses <javiesses@hotmail.com>
2020-04-07 00:13:46 -0300

    Add fix for node 8

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   presets: [
     [
-      '@babel/env',
+      '@babel/preset-env',
       {
-        modules: false,
+        modules: 'cjs',
         targets: {
+          esmodules: true,
           node: '8',
-          browsers: 'last 2 versions'
         }
       }
     ],
     '@babel/preset-typescript'
   ],
   plugins: [
-    '@babel/plugin-transform-typescript'
+    '@babel/plugin-transform-typescript',
+    'add-module-exports'
   ]
 };

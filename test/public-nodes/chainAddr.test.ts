92a869e6a21b28642e4362a2245dbdfe55eca122
Javi Esses <javiesses@hotmail.com>
2020-06-12 14:43:20 -0300

    Added new coinTypes and validate them prior setting

--- test/public-nodes/chainAddr.test.ts
+++ test/public-nodes/chainAddr.test.ts
@@ -18,30 +18,30 @@ describe.each([
   });
 
   it('should resolve a name for BTC with multichain resolver', async () => {
-    const addr = await rns.addr('multichain.testing.rsk', ChainId.BITCOIN);
+    const addr = await rns.addr('multichain.testing.rsk', ChainId.BTC);
     expect(addr).toBe('1Ftu4C8VW18RkB8PZxXwwHocMLyEynLcrG');
   });
 
   it('should resolve a name for ETH with multichain resolver', async () => {
-    const addr = await rns.addr('multichain.testing.rsk', ChainId.ETHEREUM);
+    const addr = await rns.addr('multichain.testing.rsk', ChainId.ETH);
     expect(addr).toBe('0x0000000000000000000000000000000001000006');
   });
 
   it('should throw an error when resolver has not been set', async () => {
-    await asyncExpectThrowRNSError(() => rns.addr('noresolver.multichain.testing.rsk', ChainId.BITCOIN), NO_RESOLVER);
+    await asyncExpectThrowRNSError(() => rns.addr('noresolver.multichain.testing.rsk', ChainId.BTC), NO_RESOLVER);
   });
 
   it('should throw an error when resolver does not implement chainAddr nor addr interface', async () => {
     // resolver address is the NameResolver contract. an ERC165 that not supports addr interface
-    await asyncExpectThrowRNSError(() => rns.addr('nochainaddrresolver.multichain.testing.rsk', ChainId.BITCOIN), NO_CHAIN_ADDR_RESOLUTION);
+    await asyncExpectThrowRNSError(() => rns.addr('nochainaddrresolver.multichain.testing.rsk', ChainId.BTC), NO_CHAIN_ADDR_RESOLUTION);
   });
 
   it('should throw an error when no resolution set with multichain resolver', async () => {
-    await asyncExpectThrowRNSError(() => rns.addr('noresolution.multichain.testing.rsk', ChainId.BITCOIN), NO_CHAIN_ADDR_RESOLUTION_SET);
+    await asyncExpectThrowRNSError(() => rns.addr('noresolution.multichain.testing.rsk', ChainId.BTC), NO_CHAIN_ADDR_RESOLUTION_SET);
   });
 
   it('should throw an error when domain do not exist', async () => {
-    await asyncExpectThrowRNSError(() => rns.addr('noexists.multichain.testing.rsk', ChainId.BITCOIN), NO_RESOLVER);
+    await asyncExpectThrowRNSError(() => rns.addr('noexists.multichain.testing.rsk', ChainId.BTC), NO_RESOLVER);
   });
 });
 
@@ -56,16 +56,16 @@ describe.each([
   });
 
   it('should throw an error when no resolution set with definitive resolver', async () => {
-    await asyncExpectThrowRNSError(() => rns.addr('multi.definitive.testing.rsk', ChainId.LITECOIN), NO_CHAIN_ADDR_RESOLUTION_SET);
+    await asyncExpectThrowRNSError(() => rns.addr('multi.definitive.testing.rsk', ChainId.LTC), NO_CHAIN_ADDR_RESOLUTION_SET);
   });
 
   it('should resolve a name for BTC with definitive resolver', async () => {
-    const addr = await rns.addr('multi.definitive.testing.rsk', ChainId.BITCOIN);
+    const addr = await rns.addr('multi.definitive.testing.rsk', ChainId.BTC);
     expect(addr).toBe('1Ftu4C8VW18RkB8PZxXwwHocMLyEynLcrG');
   });
 
   it('should resolve a name for ETH with definitive resolver', async () => {
-    const addr = await rns.addr('multi.definitive.testing.rsk', ChainId.ETHEREUM);
+    const addr = await rns.addr('multi.definitive.testing.rsk', ChainId.ETH);
     expect(addr).toBe('0x0000000000000000000000000000000001000006');
   });
 });
